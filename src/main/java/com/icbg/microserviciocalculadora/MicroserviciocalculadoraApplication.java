49e43de8198379f6764d5fd1cfab52dae3df8e01
icbg1978 <macuberme@gmail.com>
2020-02-04 23:35:34 +0100

    version 1

---
+++ src/main/java/com/icbg/microserviciocalculadora/MicroserviciocalculadoraApplication.java
@@ -0,0 +1,13 @@
+package com.icbg.microserviciocalculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciocalculadoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviciocalculadoraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icbg/microserviciocalculadora/MicroserviciocalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icbg.microserviciocalculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciocalculadoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
